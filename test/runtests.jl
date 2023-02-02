0cff7844fc2767e01568abf8e0ad83d629d1db05
Peter Stafford <p.stafford@me.com>
2023-02-02 15:05:10 +0000

    Fix incorrect switch on Dex and Drms

--- test/runtests.jl
+++ test/runtests.jl
@@ -672,7 +672,7 @@ using LinearAlgebra
         rvt_acr = RandomVibrationParameters(:DK80, :BT14, :BT15, :WNA)
         rvt_scr = RandomVibrationParameters(:DK80, :BT15, :BT15, :ENA)
         for i in 1:length(rti)
-            if ( rti[i] < 17.0 ) | ( rti[i] > 281.0 )
+            if (rti[i] < 17.0) | (rti[i] > 281.0)
                 @test excitation_duration(m, rti[i], src, rvt_scr) .<= excitation_duration(m, rti[i], src, rvt_acr)
             else
                 @test excitation_duration(m, rti[i], src, rvt_scr) .> excitation_duration(m, rti[i], src, rvt_acr)
@@ -760,7 +760,18 @@ using LinearAlgebra
         Drms, Dex, Dratio = StochasticGroundMotionSimulation.boore_thompson_2012(m, r, fas, sdof, rvt)
         Dex0 = StochasticGroundMotionSimulation.boore_thompson_2014(m, r, fas)
 
+        @test Dex != Dex0
+
+        rvt = RandomVibrationParameters(:CL56, :BT14, :BT12, :WNA)
+        Drms, Dex, Dratio = StochasticGroundMotionSimulation.boore_thompson_2012(m, r, fas, sdof, rvt)
+        Dex0 = StochasticGroundMotionSimulation.boore_thompson_2014(m, r, fas)
+
         @test Dex == Dex0
+
+        rvt = RandomVibrationParameters(:DK80, :BT14, :BT12, :WNA)
+        Drms, Dex, Dratio = rms_duration(m, r, fas, sdof, rvt)
+
+        @test isnan(Dex)
 
         # @code_warntype rms_duration(m, r, srcf, path, sdof, rvt)
         # @code_warntype rms_duration(m, r, srcd, path, sdof, rvt)
@@ -769,6 +780,7 @@ using LinearAlgebra
 
         # @time Drmsf, Dexf, Dratiof = rms_duration(m, r, fasf, sdof, rvt)
         # @time Drmsd, Dexd, Dratiod = rms_duration(m, r, fasd, sdof, rvt)
+        rvt = RandomVibrationParameters()
         Drmsf, Dexf, Dratiof = rms_duration(m, r, fasf, sdof, rvt)
         Drmsd, Dexd, Dratiod = rms_duration(m, r, fasd, sdof, rvt)
 
