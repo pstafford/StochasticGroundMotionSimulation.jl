9d05512025daf5f8e38163ee18eff4f412a9d512
Peter Stafford <p.stafford@me.com>
2023-07-11 11:12:22 +0100

    Implement Edwards duration/simulation suggestions

--- src/rvt/PJSrandomVibrationParameters.jl
+++ src/rvt/PJSrandomVibrationParameters.jl
@@ -10,15 +10,17 @@ Struct holding parameters/methods for Random Vibration Theory.
 - `dur_ex` is the model for excitation duration
 	- `:BT14` (default) is the Boore & Thompson (2014) model for ACRs - note that this is adpated to work with `r_ps`
 	- `:BT15` is the Boore & Thompson (2015) model for SCRs
+	- `:BE23` is an excitation duration model from Ben Edwards (2023), suggested for a South African NPP project
 - `dur_rms` is the model for rms duration
 	- `:BT12` is the Boore & Thompson (2012) model
 	- `:BT15` (default) is the Boore & Thompson (2015) model
+	- `:LP99` is the Liu & Pezeshk (1999) model linking rms, excitation and oscillator durations
 - `dur_region` is the region specified for the duration model
 	- `:ACR` (default) is active crustal regions (like western North America)
 	- `:SCR` is stable crustal regions (like eastern North America)
 
 Note that only certain combinations are meaningful:
-- `:CL56` peak factor method should be paired with `:BT12` rms duration 
+- `:CL56` peak factor method should be paired with `:BT12` or `:LP99` rms duration 
 - `:DK80` peak factor method should be paired with `:BT15` rms duration
 Constructors that take only the peak factor as input, or the peak factor and duration region automatically assign the appropriate rms duration method.
 """
