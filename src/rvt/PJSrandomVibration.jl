c54dbbbf6330c5755a5fd328ecb6d7152bdb6c23
Peter Stafford <p.stafford@me.com>
2025-11-21 15:29:59 +0000

    Comment out redundent wrapper function

--- src/rvt/PJSrandomVibration.jl
+++ src/rvt/PJSrandomVibration.jl
@@ -87,24 +87,24 @@ function rvt_response_spectral_ordinate(m::S, r_ps::T, fas::FourierParameters, s
 end
 
 
-@doc raw"""
-	rvt_response_spectral_ordinate(period::U, m::S, r_ps::T, fas::FourierParameters, rvt::RandomVibrationParameters; glxi::Vector{Float64}=xn31i, glwi::Vector{Float64}=wn31i) where {S<:Real,T<:Real,U<:Float64}
+# @doc raw"""
+# 	rvt_response_spectral_ordinate(period::U, m::S, r_ps::T, fas::FourierParameters, rvt::RandomVibrationParameters; glxi::Vector{Float64}=xn31i, glwi::Vector{Float64}=wn31i) where {S<:Real,T<:Real,U<:Float64}
 
-Response spectral ordinate (units of ``g``) for the specified scenario.
+# Response spectral ordinate (units of ``g``) for the specified scenario.
 
-The spectral ordinate is computed using the expression:
-```math
-S_a = \psi \sqrt{\frac{m_0}{D_{rms}}}
-```
-where ``\psi`` is the peak factor computed from `peak_factor`, ``m_0`` is the zeroth order spectral moment from `spectral_moment`, and ``D_{rms}`` is the RMS duration computed from `rms_duration`.
+# The spectral ordinate is computed using the expression:
+# ```math
+# S_a = \psi \sqrt{\frac{m_0}{D_{rms}}}
+# ```
+# where ``\psi`` is the peak factor computed from `peak_factor`, ``m_0`` is the zeroth order spectral moment from `spectral_moment`, and ``D_{rms}`` is the RMS duration computed from `rms_duration`.
 
-See also: [`rvt_response_spectral_ordinate`](@ref), [`rvt_response_spectrum`](@ref), [`rvt_response_spectrum!`](@ref)
-"""
-function rvt_response_spectral_ordinate(period::U, m::S, r_ps::T, fas::FourierParameters, rvt::RandomVibrationParameters; glxi::Vector{Float64}=xn31i, glwi::Vector{Float64}=wn31i, damping=0.05) where {S<:Real,T<:Real,U<:Float64}
-    # create a sdof instance
-    sdof = Oscillator(1.0 / period, damping)
-    return rvt_response_spectral_ordinate(m, r_ps, fas, sdof, rvt, glxi=glxi, glwi=glwi)
-end
+# See also: [`rvt_response_spectral_ordinate`](@ref), [`rvt_response_spectrum`](@ref), [`rvt_response_spectrum!`](@ref)
+# """
+# function rvt_response_spectral_ordinate(period::U, m::S, r_ps::T, fas::FourierParameters, rvt::RandomVibrationParameters; glxi::Vector{Float64}=xn31i, glwi::Vector{Float64}=wn31i, damping=0.05) where {S<:Real,T<:Real,U<:Float64}
+#     # create a sdof instance
+#     sdof = Oscillator(1.0 / period, damping)
+#     return rvt_response_spectral_ordinate(m, r_ps, fas, sdof, rvt, glxi=glxi, glwi=glwi)
+# end
 
 
 
